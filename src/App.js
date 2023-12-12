073363eb04dc3a6f34fc355088f6534051459cc9
Chukwuebuka98 <emmanuelzico17@gmail.com>
2023-12-12 11:48:53 +0100

    React Redux Basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const incrementCount = () => {
+    dispatch({ type: "INC" });
+  };
+
+  const decrementCount = () => {
+    dispatch({ type: "DEC" });
+  };
+
+  const addBy = () => {
+    dispatch({ type: "ADD", payLoad: 10 });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
+      <button onClick={addBy}>Add by 10</button>
     </div>
   );
 }
